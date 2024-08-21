2c2fd520b14d1e8dae49ecd4199ba31984b540b1
Gozie02 <83558859+Gozie02@users.noreply.github.com>
2024-08-21 12:52:02 +0100

    Update auto_update_organizer.py

--- auto_update_organizer.py
+++ auto_update_organizer.py
@@ -236,4 +236,4 @@ logging.info("Premier League data fetch completed")
 def run_script():
     exec(open("auto_update_matches.py").read())
 
-schedule.every().wednesday.at("11:35").do(run_script)
+schedule.every().wednesday.at("11:55").do(run_script)
