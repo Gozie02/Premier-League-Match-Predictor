54ddd79625f43d9c4d5dcfe98e83d6c9e5495de8
Gozie02 <83558859+Gozie02@users.noreply.github.com>
2024-08-27 09:37:50 +0100

    Update auto_model_retrainer.py

--- auto_model_retrainer.py
+++ auto_model_retrainer.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import BaggingClassifier
 from sklearn.metrics import accuracy_score
 import joblib
+from sklearn.impute import SimpleImputer 
 
 # Load the updated Premier League data without Outcome_encoded
 data = pd.read_csv('model_training.csv')
@@ -11,11 +12,14 @@ y = data['Outcome_encoded_home']
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+imputer = SimpleImputer(strategy='mean') # Create an imputer instance
+x_train = imputer.fit_transform(X_train) # Fit and transform on training data
+x_test = imputer.transform(X_test)
 bagging_classifier = BaggingClassifier(n_estimators=1000)
-bagging_classifier.fit(X_train, y_train)
+bagging_classifier.fit(x_train, y_train)
 
 # Evaluate the model on the testing set
-y_pred = bagging_classifier.predict(X_test)
+y_pred = bagging_classifier.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Model accuracy: {accuracy:.2f}")
 
